865c20599a2120c24c96e125b30cbcdf113bb008
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-10 12:30:51 -0700

    add reuasable form

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  // const startingName = props.merch ? props.merch.name : '';
+  // const startingDescription = props.merch ? props.merch.description : '';
+  // const startingQuantity = props.merch? props.merch.quantity : '';
+
+  return (
+    <>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          className="form-control"
+          type='text'
+          name='name'
+          placeholder='Name' 
+          // defaultValue={startingName}
+        />
+        <textarea
+          className="form-control"
+          type='text'
+          name='description'
+          placeholder='description' 
+          // defaultValue={startingDescription}
+        />
+        <input
+          className="form-control"
+          type='number'
+          name='quantity'
+          placeholder='quantity' 
+          // defaultValue={startingQuantity}
+        />
+          <button className="btn btn-warning" type='submit'>{props.buttonText}</button>
+      </form>
+    </>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string,
+  tea: PropTypes.object
+};
+
+export default ReusableForm;
