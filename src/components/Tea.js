3928aa9a37c6b1848752fa140ff8b82476b02748
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-10 10:37:31 -0700

    add Tea code

---
+++ src/components/Tea.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function Tea(props){
+  return (
+    <>
+      <div onClick = {() => props.whenTeaClicked(props.id)}>
+        <h2>Name: {props.name}</h2>
+        <h3>Brand: {props.brand}</h3>
+        <h3>Price: {props.price}</h3>
+        <h3>Flavor: {props.flavor}</h3>
+        <h3>Pints Left: {props.pints}</h3>
+        <hr />
+      </div>
+    </>
+  )
+} 
+
+Tea.propTypes = {
+  whenTeaClicked: PropTypes.func,
+  name = PropTypes.string.isRequired,
+  brand = PropTypes.string.isRequired,
+  price = PropTypes.string.isRequired,
+  flavor = PropTypes.string.isRequired,
+  pints = PropTypes.string.isRequired,
+  id = PropTypes.string.isRequired
+}
+
+export default Tea;
