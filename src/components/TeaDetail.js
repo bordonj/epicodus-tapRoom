76fbb9a3a8e8c4a6a2c8b854ff198fc0c20202c9
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-10 12:46:56 -0700

    add placeholder for tea details

---
+++ src/components/TeaDetail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function TeaDetail(props){
+  const {tea} = props;
+
+  return (
+    <>
+    </>
+  )
+
+}
+
+TeaDetail.propTypes = {
+  tea: PropTypes.object,
+};
+
+export default TeaDetail;
