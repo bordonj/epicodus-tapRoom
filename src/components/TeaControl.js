fbc83a3512c32611fbe8f891cca1ac07deb8cce6
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-10 12:46:27 -0700

    add methods to handle view changes and adding tea

--- src/components/TeaControl.js
+++ src/components/TeaControl.js
@@ -1,27 +1,60 @@
 import React from "react";
 import NewTeaForm from "./NewTeaForm";
 import TeaList from "./TeaList";
+import TeaDetail from "./TeaDetail";
 
-class TeaControl extends React.Component{
+class TeaControl extends React.Component {
   
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false,
       masterTeaList: [],
+      selectedTea: null,
     };
   }
+
+  handleClick = () => {
+    if (this.state.selectedTea != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTea: null,
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  handleAddingNewTea = (newTea) => {
+    const newMasterTeaList = this.state.masterTeaList.concat(newTea);
+    this.setState({
+      masterTeaList: newMasterTeaList,
+      formVisibleOnPage: false
+    });
+  }
+
   render(){
     let empty = null;
+    let buttonText = null;
+    let currentlyVisibleState = null;
+
     if (this.state.masterTeaList.length === 0) {
       empty = <h3>No Teas Yet</h3>
+      buttonText = "Add new Tea";
     }
 
-    let currentlyVisibleState = null;
-    let buttonText = null;
-
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTeaForm />
+    if (this.state.selectedDetail != null) {
+      currentlyVisibleState = 
+      <TeaDetail
+        tea = {this.state.selectedTea}
+      />
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = 
+      <NewTeaForm 
+        onNewTeaCreation={this.handleAddingNewTea}
+      />
       buttonText = "Return to Tea List";
     } else {
       currentlyVisibleState = 
@@ -33,11 +66,11 @@ class TeaControl extends React.Component{
     }
 
     return (
-      <>
+      <React.Fragment>
         {empty}
         {currentlyVisibleState}
-        <button className ="btn btn-primary" onClick={this.handleClick}>{buttonText}</button>
-      </>
+        <button className = "btn btn-primary" onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
     )
   }
 }
