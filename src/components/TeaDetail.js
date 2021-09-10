bcd269305d639f8055b76ea64e9733d634430113
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-10 13:18:45 -0700

    add placeholder for TeaDetail

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -4,8 +4,17 @@ import PropTypes from 'prop-types';
 function TeaDetail(props){
   const {tea} = props;
 
+  let pints = tea.pints === 0 ? "Out of Stock" : tea.pints;
+
+
   return (
     <>
+      <h2><b>Name:</b> {tea.name}</h2>
+      <h3><b>Brand:</b> {tea.brand}</h3>
+      <h3><b>Price:</b> {tea.price}</h3>
+      <h3><b>Flavor:</b> {tea.flavor}</h3>
+      <h3><b>Pints Left:</b> {pints}</h3>
+
     </>
   )
 
