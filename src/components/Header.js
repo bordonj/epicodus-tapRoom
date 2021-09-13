0a041a1680b784e8ab02c174e4c0bd3b938b09ce
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-09-12 17:15:42 -0700

    update navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,27 +5,15 @@ function Header(){
     <>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Boba Bar</a>
+          <a className="navbar-brand float-md-right" href="#">Boba Bar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDark" aria-controls="navbarDark" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse show" id="navbarDark">
-            <ul className="navbar-nav me-auto mb-2 mb-xl-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-Merch">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">Reset</a>
               </li>
-              <li className="nav-Merch">
-                <a className="nav-link" href="#">Best Sellers</a>
-              </li>
-              <li className="nav-Merch">
-                <a className="nav-link" href="#">Customer Service</a>
-              </li>
-            </ul>
-            <ul className="nav navbar-nav float-md-right">
-              <form className="d-flex">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                <button className="btn btn-outline-light" type="submit">Search</button>
-              </form>
             </ul>
           </div>
         </div>
